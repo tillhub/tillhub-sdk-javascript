729f5a375ad85839b0bf273e4d7c9e2f180fb5b7
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2019-01-02 21:39:42 +0100

    fix: dist

--- dist/types/v1/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Auth } from './auth';
-import { Products } from './products';
-import { Templates } from './templates';
-import { Transactions } from './transactions';
-import { Registers } from './registers';
-export { Auth, Products, Templates, Transactions, Registers };
