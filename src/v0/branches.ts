ade7244d12e27ee75ebe76fb86146008fea1a978
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2019-01-11 14:27:15 +0100

    feat(branches): covers api for timzeone and currency, fixes type requirements

--- src/v0/branches.ts
+++ src/v0/branches.ts
@@ -34,17 +34,18 @@ export interface Branch {
 }
 
 export interface Branch {
-  branch_number: number
+  branch_number?: number
   name: string
-  email: string
+  email?: string
   custom_id?: string
   external_custom_id?: string
-  timezone?: string
   receipt_header?: string
   receipt_footer?: string
   active?: boolean
   deleted?: boolean
   configuration?: string
+  timezone_default?: string | null
+  currency_default?: string | null
 }
 
 export class Branches {
