a3a5d9ae1a01193075a090c0750d2a48dcbc574d
Erez Sobel <sobelerez@gmail.com>
2019-01-17 18:57:09 +0100

    feat(analytics): export vat report

--- test/analytics/get_vat_report.test.ts
+++ test/analytics/get_vat_report.test.ts
@@ -64,6 +64,65 @@ describe('v0: Analytics: gets vat report', () => {
     expect(Array.isArray(data)).toBe(true)
   })
 
+  it('takes a query string', async () => {
+    const mockVatQuery = {
+      format: 'csv'
+    }
+
+    const mockString = 'format=csv'
+
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(`https://api.tillhub.com/api/v0/analytics/${legacyId}/reports/vat?${mockString}`)
+        .reply(function (config) {
+          return [
+            200,
+            {
+              count: 1,
+              results: [{}]
+            }
+          ]
+        })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    const analytics = th.analytics()
+
+    expect(analytics).toBeInstanceOf(v0.Analytics)
+
+    const { data } = await analytics.getVatReport(mockVatQuery)
+
+    expect(Array.isArray(data)).toBe(true)
+  })
+
   it('rejects on status codes that are not 200', async () => {
     if (process.env.SYSTEM_TEST !== 'true') {
       mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
