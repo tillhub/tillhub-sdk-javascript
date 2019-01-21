64af27eb18412e9f38ac663f7f23fa72e01ff699
Erez Sobel <sobelerez@gmail.com>
2019-01-21 10:46:04 +0100

    feat(staff): remove unused query params and count method; pluralise

--- test/staff/get-all.test.ts
+++ test/staff/get-all.test.ts
@@ -2,21 +2,8 @@ import * as dotenv from 'dotenv'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 dotenv.config()
-import { TillhubClient, v0 } from '../../src/tillhub-js'
-
-let user = {
-  username: 'test@example.com',
-  password: '12345678',
-  clientAccount: 'someuuid',
-  apiKey: '12345678'
-}
-
-if (process.env.SYSTEM_TEST) {
-  user.username = process.env.SYSTEM_TEST_USERNAME || user.username
-  user.password = process.env.SYSTEM_TEST_PASSWORD || user.password
-  user.clientAccount = process.env.SYSTEM_TEST_CLIENT_ACCOUNT_ID || user.clientAccount
-  user.apiKey = process.env.SYSTEM_TEST_API_KEY || user.apiKey
-}
+import { v0 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
 
 const legacyId = '4564'
 
@@ -25,7 +12,7 @@ afterEach(() => {
   mock.reset()
 })
 
-describe('v0: Staff: can get all', () => {
+describe('v0: Staff: can get all staff members', () => {
   it("Tillhub's staff are instantiable", async () => {
     if (process.env.SYSTEM_TEST !== 'true') {
       mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
@@ -52,21 +39,7 @@ describe('v0: Staff: can get all', () => {
       })
     }
 
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
+    const th = await initThInstance()
 
     const Staff = th.staff()
 
@@ -99,26 +72,11 @@ describe('v0: Staff: can get all', () => {
         })
     }
 
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
-
     try {
+      const th = await initThInstance()
       await th.staff().getAll()
     } catch (err) {
-      expect(err.name).toBe('StaffFetchFailed')
+      expect(err.name).toBe('StaffsFetchFailed')
     }
   })
 })
