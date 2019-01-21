c6df68b94f9011374b88f8bbf4c83c4d08f77bb9
jmy-kellogg <jmy.kellogg@gmail.com>
2019-01-21 16:03:46 +0100

    feat(analytics): added getCustomersReport handler in analytics

---
+++ test/analytics/get_customers_report.test.ts
@@ -0,0 +1,170 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+dotenv.config()
+import { TillhubClient, v0 } from '../../src/tillhub-js'
+
+let user = {
+  username: 'test@example.com',
+  password: '12345678',
+  clientAccount: 'someuuid',
+  apiKey: '12345678'
+}
+
+if (process.env.SYSTEM_TEST) {
+  user.username = process.env.SYSTEM_TEST_USERNAME || user.username
+  user.password = process.env.SYSTEM_TEST_PASSWORD || user.password
+  user.clientAccount = process.env.SYSTEM_TEST_CLIENT_ACCOUNT_ID || user.clientAccount
+  user.apiKey = process.env.SYSTEM_TEST_API_KEY || user.apiKey
+}
+
+const legacyId = '4564'
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+describe('v0: Analytics: gets customers report', () => {
+  it('gets customers report', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [200, { token: '', user: { id: '123', legacy_id: legacyId } }]
+      })
+
+      mock
+        .onGet(`https://api.tillhub.com/api/v0/analytics/${legacyId}/reports/customers`)
+        .reply(function (config) {
+          return [
+            200,
+            {
+              count: 1,
+              results: [{}]
+            }
+          ]
+        })
+    }
+
+    const options = {
+      credentials: { username: user.username, password: user.password },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+
+    th.init(options)
+    await th.auth.loginUsername({ username: user.username, password: user.password })
+
+    const analytics = th.analytics()
+
+    expect(analytics).toBeInstanceOf(v0.Analytics)
+
+    const { data } = await analytics.getCustomersReport()
+
+    expect(Array.isArray(data)).toBe(true)
+  })
+
+  it('takes a query string', async () => {
+    const mockCustomersQuery = {
+      format: 'csv'
+    }
+
+    const mockString = 'format=csv'
+
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(`https://api.tillhub.com/api/v0/analytics/${legacyId}/reports/customers?${mockString}`)
+        .reply(function (config) {
+          return [
+            200,
+            {
+              count: 1,
+              results: [{}]
+            }
+          ]
+        })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    const analytics = th.analytics()
+
+    expect(analytics).toBeInstanceOf(v0.Analytics)
+
+    const { data } = await analytics.getCustomersReport(mockCustomersQuery)
+
+    expect(Array.isArray(data)).toBe(true)
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(`https://api.tillhub.com/api/v0/analytics/${legacyId}/reports/customers`)
+        .reply(function (config) {
+          return [205]
+        })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    try {
+      await th.analytics().getCustomersReport()
+    } catch (err) {
+      expect(err.name).toBe('CustomerFetchFailed')
+    }
+  })
+})
