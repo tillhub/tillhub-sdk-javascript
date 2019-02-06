cc1796cf2f3578b0e4c8a4c652661402d8abc54c
Robert Lindstaedt <robert.lindstaedt@gmail.com>
2019-02-06 19:15:04 +0100

    fix(registers): fix endpoint ref

--- src/v1/registers.ts
+++ src/v1/registers.ts
@@ -67,7 +67,7 @@ export class Registers {
     this.options = options
     this.http = http
 
-    this.endpoint = '/api/v1/Registers'
+    this.endpoint = '/api/v1/registers'
     this.options.base = this.options.base || 'https://api.tillhub.com'
   }
 
