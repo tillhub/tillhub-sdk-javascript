9961c2acfda19e7360ad2c9f1a84b7d7f96b88b0
Jessica Kellogg <jessica.kellogg@tillhub.de>
2019-07-31 15:27:31 +0200

    feat(reasons): add handler v0

---
+++ test/reasons/create.spec.ts
@@ -0,0 +1,87 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+dotenv.config()
+import { v0 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+
+const legacyId = '4564'
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+const reasons = {
+  name: 'water damage',
+  description: 'box got wet',
+  type: 'stock_adjustment'
+}
+
+describe('v0: Reasons: can create a reasons', () => {
+  it("Tillhub's reasons are instantiable", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onPost(`https://api.tillhub.com/api/v0/reasons/${legacyId}`).reply(function (config) {
+        return [
+          200,
+          {
+            count: 1,
+            results: [reasons],
+            errors: []
+          }
+        ]
+      })
+    }
+
+    const th = await initThInstance()
+
+    const Reasons = th.reasons()
+
+    expect(Reasons).toBeInstanceOf(v0.Reasons)
+
+    const { data } = await Reasons.create(reasons)
+
+    expect(data).toMatchObject(reasons)
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onPost(`https://api.tillhub.com/api/v0/reasons/${legacyId}`).reply(function (config) {
+        return [205]
+      })
+    }
+
+    try {
+      const th = await initThInstance()
+      await th.reasons().create(reasons)
+    } catch (err) {
+      expect(err.name).toBe('ReasonsCreationFailed')
+    }
+  })
+})
---
+++ test/reasons/put.spec.ts
@@ -0,0 +1,91 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+dotenv.config()
+import { v0 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+
+const legacyId = '4564'
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+const reasonsId = 'asdf5566'
+const updateObject = {
+  name: 'water damage',
+  description: 'box got wet',
+  type: 'stock_adjustment'
+}
+
+describe('v0: Reasons: can alter the reasons', () => {
+  it("Tillhub's reasons are instantiable", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onPut(`https://api.tillhub.com/api/v0/reasons/${legacyId}/${reasonsId}`)
+        .reply(function (config) {
+          return [
+            200,
+            {
+              count: 1,
+              results: [updateObject]
+            }
+          ]
+        })
+    }
+
+    const th = await initThInstance()
+
+    const reasons = th.reasons()
+
+    expect(reasons).toBeInstanceOf(v0.Reasons)
+
+    const { data } = await reasons.put(reasonsId, updateObject)
+
+    expect(data).toMatchObject(updateObject)
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+      mock
+        .onPut(`https://api.tillhub.com/api/v0/reasons/${legacyId}/${reasonsId}`)
+        .reply(function (config) {
+          return [205]
+        })
+    }
+
+    const th = await initThInstance()
+
+    try {
+      await th.reasons().put(reasonsId, updateObject)
+    } catch (err) {
+      expect(err.name).toBe('ReasonsPutFailed')
+    }
+  })
+})
