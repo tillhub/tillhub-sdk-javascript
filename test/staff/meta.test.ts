181ce1a61a94071020186dc6770361c396eaae67
Quynh To Tuan <quynh.totuan@tillhub.de>
2019-10-11 17:43:25 +0200

    feat(staff): add meta query and query params

---
+++ test/staff/meta.test.ts
@@ -0,0 +1,89 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+dotenv.config()
+import { v0 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+
+const legacyId = '4564'
+
+const query = {
+  query: {
+    staff_groups: '1239812038'
+  }
+}
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+describe('v0: Staff: can get meta of staff', () => {
+  it("Tillhub's staff are instantiable", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(`https://api.tillhub.com/api/v0/staff/${legacyId}/meta?staff_groups=1239812038`)
+        .reply(function (config) {
+          return [
+            200,
+            {
+              results: [{ count: 50 }]
+            }
+          ]
+        })
+    }
+
+    const th = await initThInstance()
+
+    const Staff = th.staff()
+
+    expect(Staff).toBeInstanceOf(v0.Staff)
+
+    const { data } = await Staff.meta(query)
+
+    expect(data).toEqual({ count: 50 })
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(function (config) {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(`https://api.tillhub.com/api/v0/staff/${legacyId}/meta`)
+        .reply(function (config) {
+          return [205]
+        })
+    }
+
+    try {
+      const th = await initThInstance()
+      await th.staff().meta(query)
+    } catch (err) {
+      expect(err.name).toBe('StaffMetaFailed')
+    }
+  })
+})
