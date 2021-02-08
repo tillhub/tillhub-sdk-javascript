41fc39acd3ca058a9f927774d8c985c5cbb16a39
Erez Sobel <sobelerez@gmail.com>
2021-02-08 12:01:58 +0100

    feat: upgrade deps, remove unused files+deps, audit fix

--- examples/stories/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import Simple from '../react/simple.jsx';
-
-storiesOf('Simple', module)
-  .add('auth', () => (
-    <Simple></Simple>
-  ))
