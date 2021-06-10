0f89b31e013afe90ee8d4a8252111473a3c13551
Erez Sobel <sobelerez@gmail.com>
2021-06-10 12:11:45 +0200

    feat(v2/transactions): add handlers for getting and creating (ws) images

---
+++ src/v2/transactions.ts
@@ -0,0 +1,103 @@
+import { Client } from '../client'
+import { UriHelper } from '../uri-helper'
+import { BaseError } from '../errors/baseError'
+
+interface TransactionsOptions {
+  user?: string
+  base?: string
+}
+
+interface TransactionImageResponseItem {
+  correlationId?: string
+}
+
+interface TransactionImageCreateResponse {
+  data: TransactionImageResponseItem[]
+  metadata?: Record<string, unknown>
+}
+
+interface TransactionImageGetResponse {
+  data: TransactionImage[]
+  metadata?: Record<string, unknown>
+}
+
+interface TransactionImage {
+  original: string
+  '1x': string
+  '2x': string
+  '3x': string
+}
+
+export class Transactions {
+  endpoint: string
+  http: Client
+  public options: TransactionsOptions
+  public uriHelper: UriHelper
+
+  constructor (options: TransactionsOptions, http: Client) {
+    this.options = options
+    this.http = http
+
+    this.endpoint = '/api/v2/transactions'
+    this.options.base = this.options.base ?? 'https://api.tillhub.com'
+    this.uriHelper = new UriHelper(this.endpoint, this.options)
+  }
+
+  async getImages (transactionId: string): Promise<TransactionImageGetResponse> {
+    try {
+      const uri = this.uriHelper.generateBaseUri(`/${transactionId}/images`)
+      const response = await this.http.getClient().get(uri)
+
+      if (response.status !== 200) {
+        throw new TransactionsGetImagesFailed(undefined, { status: response.status })
+      }
+
+      if (!response.data.results[0]) throw new TransactionsGetImagesFailed()
+
+      return {
+        data: response.data.results
+      }
+    } catch (error) {
+      throw new TransactionsGetImagesFailed(undefined, { error })
+    }
+  }
+
+  async createImage (transactionId: string, image: string): Promise<TransactionImageCreateResponse> {
+    try {
+      const uri = this.uriHelper.generateBaseUri(`/${transactionId}/images`)
+      const response = await this.http.getClient().post(uri, image)
+
+      if (response.status !== 200) {
+        throw new TransactionsImageCreateFailed(undefined, { status: response.status })
+      }
+
+      return {
+        data: response.data.results
+      }
+    } catch (error) {
+      throw new TransactionsImageCreateFailed(undefined, { error })
+    }
+  }
+}
+
+export class TransactionsGetImagesFailed extends BaseError {
+  public name = 'TransactionsGetImagesFailed'
+  constructor (
+    public message: string = 'Could not get the transaction\'s images',
+    properties?: Record<string, unknown>
+  ) {
+    super(message, properties)
+    Object.setPrototypeOf(this, TransactionsGetImagesFailed.prototype)
+  }
+}
+
+export class TransactionsImageCreateFailed extends BaseError {
+  public name = 'TransactionsImageCreateFailed'
+  constructor (
+    public message: string = 'Could not create transactions image',
+    properties?: Record<string, unknown>
+  ) {
+    super(message, properties)
+    Object.setPrototypeOf(this, TransactionsImageCreateFailed.prototype)
+  }
+}
---
+++ test/transactions_v2/images.test.ts
@@ -0,0 +1,132 @@
+import { TransactionsImageCreateFailed, TransactionsGetImagesFailed } from '../../src/v2/transactions'
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import faker from 'faker'
+import MockAdapter from 'axios-mock-adapter'
+import { v2 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+dotenv.config()
+
+const legacyId = '4564'
+
+const mock = new MockAdapter(axios)
+
+beforeEach(() => {
+  if (process.env.SYSTEM_TEST !== 'true') {
+    mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() =>
+      [200, { token: '', user: { id: '123', legacy_id: legacyId } }]
+    )
+  }
+})
+
+afterEach(() => {
+  mock.reset()
+})
+
+const transactionId = faker.datatype.uuid()
+const image = faker.image.dataUri()
+
+const createImageResponse = {
+  correlationId: faker.datatype.uuid()
+}
+
+const getImagesResponse = {
+  original: faker.image.imageUrl(),
+  '1x': faker.image.imageUrl(),
+  '2x': faker.image.imageUrl(),
+  '3x': faker.image.imageUrl()
+}
+
+describe('v2: Transactions - images', () => {
+  it('can create an image', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock
+        .onPost(`https://api.tillhub.com/api/v2/transactions/${legacyId}/${transactionId}/images`)
+        .reply(() => {
+          return [
+            200,
+            {
+              count: 1,
+              results: [createImageResponse]
+            }
+          ]
+        })
+    }
+
+    const th = await initThInstance()
+
+    const transactionsV2 = th.transactionsV2()
+
+    expect(transactionsV2).toBeInstanceOf(v2.Transactions)
+
+    const { data } = await transactionsV2.createImage(transactionId, image)
+
+    expect(Array.isArray(data)).toBe(true)
+    expect(data).toEqual([createImageResponse])
+  })
+
+  it('on creating an image - rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock
+        .onPost(`https://api.tillhub.com/api/v2/transactions/${legacyId}/${transactionId}/images`)
+        .reply(() => {
+          return [205]
+        })
+    }
+
+    const th = await initThInstance()
+    const transactionsV2 = th.transactionsV2()
+
+    try {
+      await transactionsV2.createImage(transactionId, image)
+    } catch (err) {
+      expect(err.name).toBe(TransactionsImageCreateFailed.name)
+    }
+  })
+
+  it('can get images', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock
+        .onGet(`https://api.tillhub.com/api/v2/transactions/${legacyId}/${transactionId}/images`)
+        .reply(() => {
+          return [
+            200,
+            {
+              count: 1,
+              results: [getImagesResponse]
+            }
+          ]
+        })
+    }
+
+    const th = await initThInstance()
+
+    const transactionsV2 = th.transactionsV2()
+
+    expect(transactionsV2).toBeInstanceOf(v2.Transactions)
+
+    const { data } = await transactionsV2.getImages(transactionId)
+
+    expect(Array.isArray(data)).toBe(true)
+    expect(data).toEqual([getImagesResponse])
+  })
+
+  it('on getting images - rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock
+        .onGet(`https://api.tillhub.com/api/v2/transactions/${legacyId}/${transactionId}/images`)
+        .reply(() => {
+          return [205]
+        })
+    }
+
+    const th = await initThInstance()
+    const transactionsV2 = th.transactionsV2()
+
+    try {
+      await transactionsV2.getImages(transactionId)
+    } catch (err) {
+      expect(err.name).toBe(TransactionsGetImagesFailed.name)
+    }
+  })
+})
