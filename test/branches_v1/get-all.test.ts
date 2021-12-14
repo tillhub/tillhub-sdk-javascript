ea66986311f80e4225aa8afd2171bfb05a10c55a
Klemen Kastelic <klemen@olaii.com>
2021-12-14 14:15:52 +0100

    - refactored tests

--- test/branches_v1/get-all.test.ts
+++ test/branches_v1/get-all.test.ts
@@ -1,72 +1,47 @@
 import * as dotenv from 'dotenv'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import { TillhubClient, v1 } from '../../src/tillhub-js'
+import { v1 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+import faker from 'faker'
+import { BranchesFetchFailed } from '../../src/v1/branches'
 dotenv.config()
 
-const user = {
-  username: 'test@example.com',
-  password: '12345678',
-  clientAccount: 'someuuid',
-  apiKey: '12345678'
-}
+const legacyId = '4564'
 
-if (process.env.SYSTEM_TEST) {
-  user.username = process.env.SYSTEM_TEST_USERNAME ?? user.username
-  user.password = process.env.SYSTEM_TEST_PASSWORD ?? user.password
-  user.clientAccount = process.env.SYSTEM_TEST_CLIENT_ACCOUNT_ID ?? user.clientAccount
-  user.apiKey = process.env.SYSTEM_TEST_API_KEY ?? user.apiKey
-}
+const mock = new MockAdapter(axios)
 
-describe('v1: Branches: can get all', () => {
-  const legacyId = '4564'
-  const mock = new MockAdapter(axios)
-  afterEach(() => {
-    mock.reset()
-  })
-
-  it("Tillhub's branches are instantiable", async () => {
-    if (process.env.SYSTEM_TEST !== 'true') {
-      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
-        return [
-          200,
-          {
-            token: '',
-            user: {
-              id: '123',
-              legacy_id: legacyId
-            }
-          }
-        ]
-      })
-
-      mock.onGet(`https://api.tillhub.com/api/v1/branches/${legacyId}`).reply(() => {
-        return [
-          200,
-          {
-            count: 1,
-            results: [{}]
-          }
-        ]
-      })
-    }
-
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
+beforeEach(() => {
+  if (process.env.SYSTEM_TEST !== 'true') {
+    mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+      return [200, { token: '', user: { id: '123', legacy_id: legacyId } }]
     })
+  }
+})
 
+afterEach(() => {
+  mock.reset()
+})
+
+const correlationId = faker.datatype.uuid()
+
+describe('v1: Branches', () => {
+  it('can get all', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock
+        .onGet(`https://api.tillhub.com/api/v1/branches/${legacyId}`)
+        .reply(() => {
+          return [
+            200,
+            {
+              count: 1,
+              results: [{ correlationId }]
+            }
+          ]
+        })
+    }
+
+    const th = await initThInstance()
     const branches = th.branchesV1()
     expect(branches).toBeInstanceOf(v1.Branches)
 
@@ -76,44 +51,20 @@ describe('v1: Branches: can get all', () => {
 
   it('rejects on status codes that are not 200', async () => {
     if (process.env.SYSTEM_TEST !== 'true') {
-      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
-        return [
-          200,
-          {
-            token: '',
-            user: {
-              id: '123',
-              legacy_id: legacyId
-            }
-          }
-        ]
-      })
-
-      mock.onGet(`https://api.tillhub.com/api/v1/branches/${legacyId}`).reply(() => {
-        return [205]
-      })
+      mock
+        .onGet(
+          `https://api.tillhub.com/api/v1/branches/${legacyId}`
+        )
+        .reply(() => {
+          return [205]
+        })
     }
-
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
 
     try {
+      const th = await initThInstance()
       await th.branchesV1().getAll()
     } catch (err: any) {
-      expect(err.name).toBe('BranchesFetchFailed')
+      expect(err.name).toBe(BranchesFetchFailed.name)
     }
   })
 })
--- test/branches_v1/get-one.test.ts
+++ test/branches_v1/get-one.test.ts
@@ -1,47 +1,35 @@
 import * as dotenv from 'dotenv'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import { TillhubClient, v1 } from '../../src/tillhub-js'
+import { v1 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+import faker from 'faker'
+import { BranchFetchFailed } from '../../src/v1/branches'
+
 dotenv.config()
-
-const user = {
-  username: 'test@example.com',
-  password: '12345678',
-  clientAccount: 'someuuid',
-  apiKey: '12345678'
-}
-
-if (process.env.SYSTEM_TEST) {
-  user.username = process.env.SYSTEM_TEST_USERNAME ?? user.username
-  user.password = process.env.SYSTEM_TEST_PASSWORD ?? user.password
-  user.clientAccount = process.env.SYSTEM_TEST_CLIENT_ACCOUNT_ID ?? user.clientAccount
-  user.apiKey = process.env.SYSTEM_TEST_API_KEY ?? user.apiKey
-}
 
 const legacyId = '4564'
 const branchId = 'abc123'
 
 const mock = new MockAdapter(axios)
+
+beforeEach(() => {
+  if (process.env.SYSTEM_TEST !== 'true') {
+    mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+      return [200, { token: '', user: { id: '123', legacy_id: legacyId } }]
+    })
+  }
+})
+
 afterEach(() => {
   mock.reset()
 })
 
-describe('v1: Branches: can get one', () => {
-  it("Tillhub's branches are instantiable", async () => {
-    if (process.env.SYSTEM_TEST !== 'true') {
-      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
-        return [
-          200,
-          {
-            token: '',
-            user: {
-              id: '123',
-              legacy_id: legacyId
-            }
-          }
-        ]
-      })
+const correlationId = faker.datatype.uuid()
 
+describe('v1: Branches', () => {
+  it('can get one', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
       mock
         .onGet(`https://api.tillhub.com/api/v1/branches/${legacyId}/${branchId}`)
         .reply(() => {
@@ -49,77 +37,36 @@ describe('v1: Branches: can get one', () => {
             200,
             {
               count: 1,
-              results: [{ testKey: 'testKey' }]
+              results: [{ correlationId }]
             }
           ]
         })
     }
 
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
-
+    const th = await initThInstance()
     const branches = th.branchesV1()
     expect(branches).toBeInstanceOf(v1.Branches)
 
     const { data } = await branches.get(branchId)
-    expect(data).toEqual({ testKey: 'testKey' })
+    expect(data).toEqual({ correlationId })
   })
 
   it('rejects on status codes that are not 200', async () => {
     if (process.env.SYSTEM_TEST !== 'true') {
-      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
-        return [
-          200,
-          {
-            token: '',
-            user: {
-              id: '123',
-              legacy_id: legacyId
-            }
-          }
-        ]
-      })
-
       mock
-        .onGet(`https://api.tillhub.com/api/v1/branches/${legacyId}/${branchId}`)
+        .onGet(
+          `https://api.tillhub.com/api/v1/branches/${legacyId}/${branchId}`
+        )
         .reply(() => {
-          return [400]
+          return [205]
         })
     }
 
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
-
     try {
+      const th = await initThInstance()
       await th.branchesV1().get(branchId)
     } catch (err: any) {
-      expect(err.name).toBe('BranchFetchFailed')
+      expect(err.name).toBe(BranchFetchFailed.name)
     }
   })
 })
