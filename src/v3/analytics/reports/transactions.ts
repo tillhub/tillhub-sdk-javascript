bb449b27956f55cec697b19fe8963df7081db753
Erez Sobel <sobelerez@gmail.com>
2022-01-24 11:42:02 +0100

    feat(v3/transactions): add get all handler

--- src/v3/analytics/reports/transactions.ts
+++ src/v3/analytics/reports/transactions.ts
@@ -1,31 +1,33 @@
-import { ThAnalyticsBaseHandler } from '../../../base'
+import {
+  ThAnalyticsBaseHandler,
+  ThAnalyticsBaseResultItem,
+  AnalyticsSocketsExportResponseItem
+} from '../../../base'
 import { Client } from '../../../client'
 import { BaseError } from '../../../errors'
 import { UriHelper } from '../../../uri-helper'
+import { AnalyticsOptions } from '../../../v0/analytics'
 
-export interface TransactionsHandlerOptions {
-  user?: string
-  base?: string
-}
-
-export interface AnalyticsReportsTransactionsV3ExportResponseItem {
-  correlationId?: string
-}
-
-export interface AnalyticsResponse {
-  data: AnalyticsReportsTransactionsV3ExportResponseItem[]
-  metadata: Record<string, unknown>
-  msg?: string
+export interface AnalyticsReportsTransactionsOverviewResponseItem {
+  data: Array<Record<string, unknown>>
+  summary: Array<Record<string, unknown>>
+  metaData: {
+    count: number
+    total_count: number
+  }
+  next?: () => Promise<AnalyticsReportsTransactionsOverviewResponseItem>
 }
 
 export class AnalyticsReportsTransactions extends ThAnalyticsBaseHandler {
   http: Client
-  public options: TransactionsHandlerOptions
+  public options: AnalyticsOptions
+  public timeout: AnalyticsOptions['timeout']
 
-  constructor (options: TransactionsHandlerOptions, http: Client) {
+  constructor (options: AnalyticsOptions, http: Client) {
     super(http, options)
     this.options = options
     this.http = http
+    this.timeout = options.timeout ?? this.http.getClient().defaults.timeout
   }
 
   static create (options: Record<string, unknown>, http: Client): AnalyticsReportsTransactions {
@@ -36,25 +38,81 @@ export class AnalyticsReportsTransactions extends ThAnalyticsBaseHandler {
     )
   }
 
+  public async getAll (
+    query?: Record<string, unknown>
+  ): Promise<AnalyticsReportsTransactionsOverviewResponseItem> {
+    try {
+      let nextFn
+      const localUriHelper = new UriHelper('/api/v3/analytics', this.options)
+      const uri = localUriHelper.generateBaseUri('/reports/transactions/overview')
+      const { results: d, next } = await this.handleGet(uri, query, { timeout: this.timeout })
+
+      if (!d) {
+        throw new TypeError('Unexpectedly did not return data.')
+      }
+
+      const data = d?.find(
+        (item: ThAnalyticsBaseResultItem) =>
+          item.metric.job === 'reports_transactions_v2_overview_data'
+      )?.values ?? []
+
+      const summary = d?.find(
+        (item: ThAnalyticsBaseResultItem) =>
+          item.metric.job === 'reports_transactions_v2_overview_summary'
+      )?.values ?? []
+
+      const count = d?.find(
+        (item: ThAnalyticsBaseResultItem) =>
+          item.metric.job === 'reports_transactions_v2_overview_filtered_meta'
+      )?.values[0] ?? {}
+
+      const totalCount = d?.find(
+        (item: ThAnalyticsBaseResultItem) =>
+          item.metric.job === 'reports_transactions_v2_overview_meta'
+      )?.values[0] ?? {}
+
+      if (next) {
+        nextFn = (): Promise<AnalyticsReportsTransactionsOverviewResponseItem> =>
+          this.getAll({ uri: next })
+      }
+
+      return {
+        data: data,
+        summary: summary,
+        metaData: {
+          count: count.count,
+          total_count: totalCount.count
+        },
+        next: nextFn
+      }
+    } catch (error: any) {
+      throw new AnalyticsReportsTransactionsOverviewFetchError(error.message, { error })
+    }
+  }
+
   public async export (
     query?: Record<string, unknown>
-  ): Promise<AnalyticsResponse> {
+  ): Promise<AnalyticsSocketsExportResponseItem> {
     try {
       const localUriHelper = new UriHelper('/api/v3/analytics', this.options)
-      const base = localUriHelper.generateBaseUri('/reports/transactions/overview')
-      const uri = localUriHelper.generateUriWithQuery(base, query)
+      const uri = localUriHelper.generateBaseUri('/reports/transactions/overview')
+      const result = await this.handleSocketsExport(uri, query)
 
-      const response = await this.http.getClient().get(uri)
-      if (response.status !== 200) throw new AnalyticsReportsV3TransactionsExportFetchError()
-      return {
-        data: response.data.results,
-        metadata: {
-          count: response.data.count
-        }
-      }
+      return result
     } catch (error: any) {
       throw new AnalyticsReportsV3TransactionsExportFetchError(error.message, { error })
     }
+  }
+}
+
+export class AnalyticsReportsTransactionsOverviewFetchError extends BaseError {
+  public name = 'AnalyticsReportsTransactionsOverviewFetchError'
+  constructor (
+    public message: string = 'Could not fetch transaction overview. ',
+    properties?: Record<string, unknown>
+  ) {
+    super(message, properties)
+    Object.setPrototypeOf(this, AnalyticsReportsTransactionsOverviewFetchError.prototype)
   }
 }
 
