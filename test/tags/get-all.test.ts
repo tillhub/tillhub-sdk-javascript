df551bf0584f1ba805993463bf6e28f8e6a93b1f
maurogri-th <73841736+maurogri-th@users.noreply.github.com>
2022-02-22 15:52:07 +0100

    feat(tags): refactor v1  code (#419)

--- test/tags/get-all.test.ts
+++ test/tags/get-all.test.ts
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
-import { TillhubClient, v0, v1 } from '../../src/tillhub-js'
+import { TillhubClient, v0 } from '../../src/tillhub-js'
 dotenv.config()
 
 const user = {
@@ -114,101 +114,6 @@ describe('v0: Tags: can get all', () => {
 
     try {
       await th.tags().getAll()
-    } catch (err: any) {
-      expect(err.name).toBe('TagsFetchAllFailed')
-    }
-  })
-})
-
-describe('v1: Tags: can get all', () => {
-  it("Tillhub's Tags are instantiable", async () => {
-    if (process.env.SYSTEM_TEST !== 'true') {
-      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
-        return [
-          200,
-          {
-            token: '',
-            user: {
-              id: '123',
-              legacy_id: legacyId
-            }
-          }
-        ]
-      })
-
-      mock.onGet(`https://api.tillhub.com/api/v1/tags/${legacyId}`).reply(() => {
-        return [
-          200,
-          {
-            count: 1,
-            results: [{}]
-          }
-        ]
-      })
-    }
-
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
-
-    const tags = th.tagsV1()
-
-    expect(tags).toBeInstanceOf(v1.Tags)
-
-    const { data } = await tags.getAll()
-
-    expect(Array.isArray(data)).toBe(true)
-  })
-
-  it('rejects on status codes that are not 200', async () => {
-    if (process.env.SYSTEM_TEST !== 'true') {
-      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
-        return [
-          200,
-          {
-            token: '',
-            user: {
-              id: '123',
-              legacy_id: legacyId
-            }
-          }
-        ]
-      })
-      mock.onGet(`https://api.tillhub.com/api/v1/tags/${legacyId}`).reply(() => {
-        return [205]
-      })
-    }
-
-    const options = {
-      credentials: {
-        username: user.username,
-        password: user.password
-      },
-      base: process.env.TILLHUB_BASE
-    }
-
-    const th = new TillhubClient()
-
-    th.init(options)
-    await th.auth.loginUsername({
-      username: user.username,
-      password: user.password
-    })
-
-    try {
-      await th.tagsV1().getAll()
     } catch (err: any) {
       expect(err.name).toBe('TagsFetchAllFailed')
     }
