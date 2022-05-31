9dd7dc271771160240ab75c671390b43ec509919
Roy <roy.almaliach@tillhub.de>
2022-05-31 14:32:31 +0200

    feat(customers): migrate customers API to V1

--- src/v1/customers.ts
+++ src/v1/customers.ts
@@ -2,10 +2,20 @@ import { Client } from '../client'
 import { UriHelper } from '../uri-helper'
 import { ThBaseHandler } from '../base'
 import {
+  Customer,
+  CustomerCreationFailed,
+  CustomerDeleteFailed,
+  CustomerFetchFailed,
+  CustomerPutFailed,
+  CustomerResponse,
+  CustomersCountFailed,
+  CustomersFetchFailed,
+  CustomersMetaFailed,
+  CustomersMetaQuery,
   CustomersOptions,
   CustomersQuery,
   CustomersResponse,
-  CustomersFetchFailed
+  HandlerCustomerQuery
 } from '../v0/customers'
 
 export class Customers extends ThBaseHandler {
@@ -50,6 +60,115 @@ export class Customers extends ThBaseHandler {
       }
     } catch (error: any) {
       throw new CustomersFetchFailed(error.message, { error })
+    }
+  }
+
+  async get (customerId: string, query: CustomersQuery): Promise<CustomerResponse> {
+    const base = this.uriHelper.generateBaseUri(`/${customerId}`)
+    const uri = this.uriHelper.generateUriWithQuery(base, query)
+
+    try {
+      const response = await this.http.getClient().get(uri)
+
+      if (response.status !== 200) {
+        throw new CustomerFetchFailed(undefined, { status: response.status })
+      }
+      return {
+        data: response.data.results[0] as Customer,
+        msg: response.data.msg,
+        metadata: { count: response.data.count }
+      }
+    } catch (error: any) {
+      throw new CustomerFetchFailed(error.message, { error })
+    }
+  }
+
+  async create (customer: Customer, query?: HandlerCustomerQuery): Promise<CustomerResponse> {
+    const base = this.uriHelper.generateBaseUri()
+    const uri = this.uriHelper.generateUriWithQuery(base, query)
+    try {
+      const response = await this.http.getClient().post(uri, customer)
+
+      if (response.status !== 200) {
+        throw new CustomerCreationFailed(undefined, { status: response.status })
+      }
+      return {
+        data: response.data.results[0] as Customer,
+        metadata: { count: response.data.count },
+        errors: response.data.errors || []
+      }
+    } catch (error: any) {
+      throw new CustomerCreationFailed(error.message, { error })
+    }
+  }
+
+  async put (customerId: string, customer: Customer): Promise<CustomerResponse> {
+    const uri = this.uriHelper.generateBaseUri(`/${customerId}`)
+    try {
+      const response = await this.http.getClient().put(uri, customer)
+
+      if (response.status !== 200) {
+        throw new CustomerPutFailed(undefined, { status: response.status })
+      }
+      return {
+        data: response.data.results[0] as Customer,
+        metadata: { count: response.data.count }
+      }
+    } catch (error: any) {
+      throw new CustomerPutFailed(error.message, { error })
+    }
+  }
+
+  async meta (q?: CustomersMetaQuery | undefined): Promise<CustomersResponse> {
+    const base = this.uriHelper.generateBaseUri('/meta')
+    const uri = this.uriHelper.generateUriWithQuery(base, q)
+    try {
+      const response = await this.http.getClient().get(uri)
+      if (response.status !== 200) {
+        throw new CustomersMetaFailed(undefined, { status: response.status })
+      }
+      if (!response.data.results[0]) {
+        throw new CustomersMetaFailed(undefined, { status: response.status })
+      }
+
+      return {
+        data: response.data.results[0],
+        metadata: { count: response.data.count }
+      }
+    } catch (error: any) {
+      throw new CustomersMetaFailed(error.message, { error })
+    }
+  }
+
+  async delete (customerId: string): Promise<CustomerResponse> {
+    const uri = this.uriHelper.generateBaseUri(`/${customerId}`)
+    try {
+      const response = await this.http.getClient().delete(uri)
+      if (response.status !== 200) {
+        throw new CustomerDeleteFailed(undefined, { status: response.status })
+      }
+      return {
+        msg: response.data.msg
+      }
+    } catch (error: any) {
+      throw new CustomerDeleteFailed(error.message, { error })
+    }
+  }
+
+  async count (): Promise<CustomersResponse> {
+    const uri = this.uriHelper.generateBaseUri('/meta')
+
+    try {
+      const response = await this.http.getClient().get(uri)
+      if (response.status !== 200) {
+        throw new CustomersCountFailed(undefined, { status: response.status })
+      }
+      return {
+        data: response.data.results,
+        metadata: { count: response.data.count }
+      }
+    } catch (error: any) {
+      throw new CustomersCountFailed(error.message, { error })
     }
   }
 }
---
+++ test/customers-v1/create.test.ts
@@ -0,0 +1,101 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import qs from 'qs'
+import { v1 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+dotenv.config()
+
+const legacyId = '4564'
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+const customer = {
+  firstname: 'Carol',
+  lastname: 'Danvers',
+  gender: 'female',
+  company: {
+    name: 'US Air Force'
+  },
+  active: false,
+  displayname: 'Vers'
+}
+
+describe('v0: Customers: can create a customer', () => {
+  const query = {
+    customer_number_template: '{country}{-}{branch}',
+    generate_customer_number: true
+  }
+
+  it("Tillhub's customers are instantiable", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onPost(`https://api.tillhub.com/api/v1/customers/${legacyId}?${qs.stringify(query)}`)
+        .reply(() => {
+          return [
+            200,
+            {
+              count: 1,
+              results: [customer],
+              errors: []
+            }
+          ]
+        })
+    }
+
+    const th = await initThInstance()
+
+    const Customers = th.customersV1()
+
+    expect(Customers).toBeInstanceOf(v1.Customers)
+
+    const { data, errors } = await Customers.create(customer, { query })
+
+    expect(data).toMatchObject(customer)
+    expect(errors).toEqual([])
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onPost(`https://api.tillhub.com/api/v1/customers/${legacyId}`).reply(() => {
+        return [205]
+      })
+    }
+
+    try {
+      const th = await initThInstance()
+      await th.customersV1().create(customer)
+    } catch (err: any) {
+      expect(err.name).toBe('CustomerCreationFailed')
+    }
+  })
+})
