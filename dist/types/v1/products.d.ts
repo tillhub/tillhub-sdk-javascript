27d0b752d500ebb63e4b6aa940fb12fe7003ea6a
Florian Pirmin Groetzner <florian.groetzner@gmail.com>
2023-05-31 22:05:47 +0200

    Add missing reason to BookStock

--- dist/types/v1/products.d.ts
+++ dist/types/v1/products.d.ts
@@ -141,6 +141,7 @@ export interface BookStockQuery {
 export interface BookStock {
     location: string;
     qty: number;
+    reason?: string
 }
 export interface BarcodeResponse {
     barcode?: string;
--- src/v1/products.ts
+++ src/v1/products.ts
@@ -163,6 +163,7 @@ export interface BookStockQuery {
 export interface BookStock {
   location: string
   qty: number
+  reason?: string
 }
 
 export interface BarcodeResponse {
