9e90369880d341c8d82c5b2f53b9faf0601f110d
Dejan Nesic <desicne@gmail.com>
2023-06-06 08:17:26 +0200

    fix(UNTIL-6762): add test for fetching one order

---
+++ test/orders_v2/get-one-order.test.ts
@@ -0,0 +1,125 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { TillhubClient, v2 } from '../../src/tillhub-js'
+dotenv.config()
+
+const user = {
+  username: 'test@example.com',
+  password: '12345678',
+  clientAccount: 'someuuid',
+  apiKey: '12345678'
+}
+
+if (process.env.SYSTEM_TEST) {
+  user.username = process.env.SYSTEM_TEST_USERNAME ?? user.username
+  user.password = process.env.SYSTEM_TEST_PASSWORD ?? user.password
+  user.clientAccount = process.env.SYSTEM_TEST_CLIENT_ACCOUNT_ID ?? user.clientAccount
+  user.apiKey = process.env.SYSTEM_TEST_API_KEY ?? user.apiKey
+}
+
+describe('v2: orders: can get one', () => {
+  const legacyId = '4564'
+  const orderId = '432053'
+  const mock = new MockAdapter(axios)
+  afterEach(() => {
+    mock.reset()
+  })
+
+  it("Tillhub's orders are instantiable", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onGet(`https://api.tillhub.com/api/v2/orders/${legacyId}/${orderId}`).reply(() => {
+        return [
+          200,
+          {
+            count: 1,
+            results: [{ id: '12345' }]
+          }
+        ]
+      })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    const ordersV2 = th.ordersV2()
+
+    expect(ordersV2).toBeInstanceOf(v2.Orders)
+
+    try {
+      const { data } = await ordersV2.get(legacyId, orderId)
+      expect(data).toEqual({ id: '12345' })
+    } catch (err: any) {
+      expect(err.name).toBe('OrderFetchFailed')
+    }
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onGet(`https://api.tillhub.com/api/v2/orders/${legacyId}/${orderId}`).reply(() => {
+        return [205]
+      })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    try {
+      await th.ordersV2().get(legacyId, orderId)
+    } catch (err: any) {
+      expect(err.name).toBe('OrderFetchFailed')
+    }
+  })
+})
