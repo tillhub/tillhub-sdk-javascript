a74c6ef77db5a5ec116fdb909abc5db11c796ae5
Vladimir <vladimir.vancea@hotmail.com>
2023-09-05 15:37:45 +0300

    feat(stock management): add getAll method for consignment notes entity

--- src/v0/consignment_notes.ts
+++ src/v0/consignment_notes.ts
@@ -8,6 +8,34 @@ export interface ConsignmentNotesOptions {
   base?: string
 }
 
+export interface ConsignmentNotesResponse {
+  data?: ConsignmentNote[]
+  metadata?: Record<string, unknown>
+  msg?: string
+  next?: () => Promise<ConsignmentNotesResponse>
+}
+
+export interface ConsignmentNote {
+  id: string
+  createdAt?: string
+  createdBy?: string
+  purchaseOrder?: {
+    id?: string
+  }
+  consignmentNoteNumber: string
+}
+
+export interface ConsignmentNotesOptions {
+  base?: string
+  limit?: number
+  uri?: string
+  query?: ConsignmentNotesQuery
+}
+
+export interface ConsignmentNotesQuery {
+  consignmentNoteNumber?: string
+  [key: string]: any
+}
 export interface ConsignmentNotesPdfResponse {
   data?: string
   contentType?: string
@@ -21,7 +49,7 @@ export class ConsignmentNotes extends ThBaseHandler {
   public options: ConsignmentNotesOptions
   public uriHelper: UriHelper
 
-  constructor (options: ConsignmentNotesOptions, http: Client) {
+  constructor(options: ConsignmentNotesOptions, http: Client) {
     super(http, {
       endpoint: ConsignmentNotes.baseEndpoint,
       base: options.base ?? 'https://api.tillhub.com'
@@ -34,7 +62,35 @@ export class ConsignmentNotes extends ThBaseHandler {
     this.uriHelper = new UriHelper(this.endpoint, this.options)
   }
 
-  async pdfUri (consignmentNoteId: string): Promise<ConsignmentNotesPdfResponse> {
+  async getAll(options?: ConsignmentNotesOptions | undefined): Promise<ConsignmentNotesResponse> {
+    let next
+
+    try {
+      const base = this.uriHelper.generateBaseUri()
+      const uri = this.uriHelper.generateUriWithQuery(base, options)
+
+      const response = await this.http.getClient().get(uri)
+
+      if (response.status !== 200) {
+        throw new ConsignmentNotesFetchFailed(undefined, { status: response.status })
+      }
+
+      if (response.data.cursor?.next) {
+        next = (): Promise<ConsignmentNotesResponse> =>
+          this.getAll({ uri: response.data.cursor.next })
+      }
+
+      return {
+        data: response.data.results,
+        metadata: { count: response.data.count, cursor: response.data.cursor },
+        next
+      }
+    } catch (error: any) {
+      throw new ConsignmentNotesFetchFailed(error.message, { error })
+    }
+  }
+
+  async pdfUri(consignmentNoteId: string): Promise<ConsignmentNotesPdfResponse> {
     try {
       const base = this.uriHelper.generateBaseUri(`/${consignmentNoteId}/pdf`)
       const uri = this.uriHelper.generateUriWithQuery(base)
@@ -53,9 +109,19 @@ export class ConsignmentNotes extends ThBaseHandler {
   }
 }
 
+export class ConsignmentNotesFetchFailed extends BaseError {
+  public name = 'ConsignmentNotesFetchFailed'
+  constructor(
+    public message: string = 'Could not fetch the consignment notes',
+    properties?: Record<string, unknown>
+  ) {
+    super(message, properties)
+    Object.setPrototypeOf(this, ConsignmentNotesFetchFailed.prototype)
+  }
+}
 class ConsignmentNotesPdfFailed extends BaseError {
   public name = 'ConsignmentNotesPdfFailed'
-  constructor (
+  constructor(
     public message: string = 'Could not download pdf',
     properties?: Record<string, unknown>
   ) {
