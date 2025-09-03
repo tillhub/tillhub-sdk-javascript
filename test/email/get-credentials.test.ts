ad7b3d050cb3826ee93a354cf0b1c65e40552c62
Nattaphong Kosakul <nattaphong.kosakul@unzer.com>
2025-09-03 12:28:50 +0200

    feat(integrations): add tests for email handler (UNTIL 14800)

---
+++ test/email/get-credentials.test.ts
@@ -0,0 +1,129 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { TillhubClient, v0 } from '../../src/tillhub-js'
+dotenv.config()
+
+const user = {
+  username: 'test@example.com',
+  password: '12345678',
+  clientAccount: 'someuuid',
+  apiKey: '12345678'
+}
+
+if (process.env.SYSTEM_TEST) {
+  user.username = process.env.SYSTEM_TEST_USERNAME ?? user.username
+  user.password = process.env.SYSTEM_TEST_PASSWORD ?? user.password
+  user.clientAccount = process.env.SYSTEM_TEST_CLIENT_ACCOUNT_ID ?? user.clientAccount
+  user.apiKey = process.env.SYSTEM_TEST_API_KEY ?? user.apiKey
+}
+
+const legacyId = '4564'
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+describe('v0: Email: can get credentials', () => {
+  const mockCredentialsResult = {
+    apiKey: 'test-api-key',
+    apiSecret: 'test-api-secret',
+    email: 'test@example.com',
+    name: 'Test User'
+  }
+
+  it("Tillhub's email handler can get credentials", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onGet(`https://api.tillhub.com/api/v0/email/${legacyId}/credentials`).reply(() => {
+        return [
+          200,
+          {
+            results: [mockCredentialsResult],
+            status: 200,
+            msg: 'Success'
+          }
+        ]
+      })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    const email = th.email()
+    expect(email).toBeInstanceOf(v0.Email)
+
+    const { data, status, msg } = await email.getCredentials()
+
+    expect(Array.isArray(data)).toBe(true)
+    expect(status).toBe(200)
+    expect(msg).toBe('Success')
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock.onGet(`https://api.tillhub.com/api/v0/email/${legacyId}/credentials`).reply(() => {
+        return [500]
+      })
+    }
+
+    const options = {
+      credentials: {
+        username: user.username,
+        password: user.password
+      },
+      base: process.env.TILLHUB_BASE
+    }
+
+    const th = new TillhubClient()
+    th.init(options)
+    await th.auth.loginUsername({
+      username: user.username,
+      password: user.password
+    })
+
+    try {
+      await th.email().getCredentials()
+    } catch (err: any) {
+      expect(err.name).toBe('EmailCredentialsFetchFailed')
+    }
+  })
+})
