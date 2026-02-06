3870d041b78a1f98576edb8d01665c5f9a17a26e
Mauro Gri <mauro.gri@tillhub.de>
2026-02-06 10:48:47 +0100

    feat(transaction_batches): add endpoints - UOD-2848

---
+++ test/transaction_batches/download-input.test.ts
@@ -0,0 +1,90 @@
+import * as dotenv from 'dotenv'
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import { v0 } from '../../src/tillhub-js'
+import { initThInstance } from '../util'
+dotenv.config()
+
+const legacyId = '4564'
+const batchId = '123'
+
+const mock = new MockAdapter(axios)
+afterEach(() => {
+  mock.reset()
+})
+
+const blobContent = 'original-input-file-content'
+
+describe('v0: TransactionBatches: can download input file', () => {
+  it("Tillhub's transaction batches downloadInput returns blob", async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(
+          `https://api.tillhub.com/api/v0/transaction-batches/${legacyId}/${batchId}/download-input`
+        )
+        .reply((config) => {
+          if (config.responseType === 'blob') {
+            return [200, blobContent]
+          }
+          return [400, 'Bad input']
+        })
+    }
+
+    const th = await initThInstance()
+    const transactionBatches = th.transactionBatches()
+
+    expect(transactionBatches).toBeInstanceOf(v0.TransactionBatches)
+
+    const result = await transactionBatches.downloadInput(batchId)
+
+    expect(result).toBeDefined()
+    if (process.env.SYSTEM_TEST !== 'true') {
+      expect(result).toEqual(blobContent)
+    }
+  })
+
+  it('rejects on status codes that are not 200', async () => {
+    if (process.env.SYSTEM_TEST !== 'true') {
+      mock.onPost('https://api.tillhub.com/api/v0/users/login').reply(() => {
+        return [
+          200,
+          {
+            token: '',
+            user: {
+              id: '123',
+              legacy_id: legacyId
+            }
+          }
+        ]
+      })
+
+      mock
+        .onGet(
+          `https://api.tillhub.com/api/v0/transaction-batches/${legacyId}/${batchId}/download-input`
+        )
+        .reply(() => {
+          return [404]
+        })
+    }
+
+    try {
+      const th = await initThInstance()
+      await th.transactionBatches().downloadInput(batchId)
+    } catch (err: any) {
+      expect(err.name).toBe('TransactionBatchDownloadInputFailed')
+    }
+  })
+})
