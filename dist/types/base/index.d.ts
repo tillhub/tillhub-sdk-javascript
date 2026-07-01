235b363fa9e0a1a19f1a33d4b045e52521b662c7
Bogdan Mihai-Bucur <bogdan.bucur@phantomsolutions.org>
2026-07-01 14:06:45 +0300

    refactor: removes dist directory

--- dist/types/base/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ThBaseHandler, ThAnalyticsBaseHandler, ThAnalyticsBaseResponse, ThAnalyticsExportsBaseResponse, ThAnalyticsBaseResultItem, AnalyticsSocketsExportResponseItem } from './handler';
-import { ThBaseRequest } from './request';
-export { ThBaseHandler, ThBaseRequest, ThAnalyticsBaseHandler, ThAnalyticsBaseResponse, ThAnalyticsExportsBaseResponse, ThAnalyticsBaseResultItem, AnalyticsSocketsExportResponseItem };
--- dist/types/v4/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Analytics } from './analytics';
-import { AnalyticsUod } from './analytics-uod';
-import { Products } from './products';
-export { Analytics, AnalyticsUod, Products };
