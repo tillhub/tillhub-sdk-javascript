9b3bf1edc7fbc771a1aeed8d81714aa65b161dac
Barbara <b.mecaj.ext@unzer.com>
2026-09-08 15:16:20 +0200

    feat(language): add language field to IamUser interface and update test data - UOD-3610

--- src/v0/iam_users.ts
+++ src/v0/iam_users.ts
@@ -40,6 +40,7 @@ export interface IamUser {
   username?: string
   firstName?: string
   lastName?: string
+  language?: string | null
   attributes?: Record<string, unknown>
   groups?: string[]
   enabled?: boolean
